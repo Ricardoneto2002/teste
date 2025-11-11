2e2118e4f37551f55e014a0048105ed3efa6b3e3
Ricardo Neto <ricardosimoesneto@gmail.com>
2025-11-11 17:56:15 +0000

    Remove all Groovy pipeline scripts

--- Detections_Filled.groovy
+++
@@ -1,6 +0,0 @@
-def annotations = getCellObjects()
-def detections = annotations.collect {
-    PathObjects.createDetectionObject(it.getROI(), it.getPathClass(), it.getMeasurementList())
-}
-
-addObjects(detections)
